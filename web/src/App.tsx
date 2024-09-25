8c25743cfc0f853087194ab46a06a2dbe6f14fff
tabe0127 <s2312869@u.tsukuba.ac.jp>
2024-09-25 15:20:45 +0900

    Hello World
    環境構築

---
+++ web/src/App.tsx
@@ -0,0 +1,39 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Try editing me! ?</Text>
+    </View>
+  );
+};
+
+export default App;
